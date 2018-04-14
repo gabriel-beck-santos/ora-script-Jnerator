22af73eef240c362f564e3a3718e00589549d5a0
Alex S. Rosa <alexsros@gmail.com>
2018-04-14 19:06:36 -0300

    Inclusão de combo para selecao do template e exibicao dinamica na tela.

--- src/main/java/com/ora/script/jnerator/controller/WebController.java
+++ src/main/java/com/ora/script/jnerator/controller/WebController.java
@@ -2,11 +2,10 @@ package com.ora.script.jnerator.controller;
 
 import com.ora.script.jnerator.domain.Field;
 import com.ora.script.jnerator.domain.KeyValue;
+import com.ora.script.jnerator.domain.TemplateSql;
 import com.ora.script.jnerator.processor.ReadTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
@@ -18,12 +17,11 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
 
 /**
  * @author <a href="mailto:gabriel.santos@ntconsult.com.br">gabriel.santos</a>
@@ -39,11 +37,41 @@ public class WebController {
     }
 
     @RequestMapping("/")
-    public String welcome(Model model) {
+    public String index(WebRequest webRequest, Model model) {
 
+        listTemplates(model);
+
+        Map<String, String[]> map = webRequest.getParameterMap();
+        map.forEach((s, strings) -> System.out.println("Chave: " + s +" - Valor:" + strings[0]));
+
+        String templateSelected = "";
+        try{
+            templateSelected = map.get("templateOptions")[0];
+            model.addAttribute("templateSelected", templateSelected);
+        }catch (Exception e){
+            model.addAttribute("templateSelected", "");
+        }
+
+        carregaTemplateSelecionado(model, templateSelected);
+
+        Map<String, String> var = new HashMap<>();
+
+        var.put("#{descricao}", "Descrição");
+        var.put("#{autor}", "Gabriel Beck dos Santos");
+        var.put("#{data}", LocalDate.now().toString());
+        var.put("#{tabela}", "SEFAZ_CCI.TA_DOCUMENTO_APRESENTADO");
+        var.put("#{alter_script}", "SEFAZ_CCI.TA_DOCUMENTO_APRESENTADO ADD IDEN_PENDENCIA NUMBER(12, 0)");
+
+        model.addAttribute("final", readTemplate.generateSqlTemplate(Paths.get("sql-templates/DDL/ALTER/demo.txt"), var));
+        return "index";
+    }
+
+    private void carregaTemplateSelecionado(Model model, String templateSelected) {
         Field field = new Field();
         try {
-            Stream<String> lines = Files.lines(Paths.get("sql-templates/DDL/ALTER/demo.txt"));
+
+            TemplateSql template = TemplateSql.getTemplateBy(templateSelected);
+            Stream<String> lines = Files.lines(Paths.get(template.getPath()));
 
             List<String> collect = lines.collect(Collectors.toList());
 
@@ -59,20 +87,16 @@ public class WebController {
 
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (IllegalArgumentException e){
+            e.printStackTrace();
         }
 
-        Map<String, String> var = new HashMap<>();
+        model.addAttribute("field", Objects.isNull(field) ? new Field() : field);
+    }
 
-        var.put("#{descricao}", "Descrição");
-        var.put("#{autor}", "Gabriel Beck dos Santos");
-        var.put("#{data}", LocalDate.now().toString());
-        var.put("#{tabela}", "SEFAZ_CCI.TA_DOCUMENTO_APRESENTADO");
-        var.put("#{alter_script}", "SEFAZ_CCI.TA_DOCUMENTO_APRESENTADO ADD IDEN_PENDENCIA NUMBER(12, 0)");
-
-        model.addAttribute("field", field);
-
-        model.addAttribute("final", readTemplate.generateSqlTemplate(Paths.get("sql-templates/DDL/ALTER/demo.txt"), var));
-        return "index";
+    private void listTemplates(Model model) {
+        List<TemplateSql> listTemplates = TemplateSql.getListTemplates();
+        model.addAttribute("templateOptions", listTemplates);
     }
 
     @PostMapping("/generate")
@@ -82,7 +106,6 @@ public class WebController {
 
         map.forEach((s, strings) -> System.out.println("Chave: " + s +" - Valor:" + strings));
 
-        System.out.println("CHEGA aqui");
         return "index";
     }
 }
--- src/main/java/com/ora/script/jnerator/domain/TemplateSql.java
+++ src/main/java/com/ora/script/jnerator/domain/TemplateSql.java
@@ -9,16 +9,16 @@ public enum TemplateSql {
     TEMPLATE_DDL_DROP_DEMO("TEMPLATE_DDL_DROP_DEMO", "sql-templates/DDL/DROP/demo.txt"),
     TEMPLATE_DDL_CREATE_DEMO("TEMPLATE_DDL_CREATE_DEMO", "sql-templates/DDL/CREATE/demo.txt");
 
-    private final String nome;
+    private final String name;
     private final String path;
 
-    TemplateSql(String nome, String path){
-        this.nome = nome;
+    TemplateSql(String name, String path){
+        this.name = name;
         this.path = path;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
     public String getPath() {
@@ -27,5 +27,15 @@ public enum TemplateSql {
 
     public static List<TemplateSql> getListTemplates(){
         return Arrays.asList(TemplateSql.values());
+    }
+
+    public static TemplateSql getTemplateBy(String s) {
+        for (TemplateSql template : TemplateSql.values()) {
+            if (template.getName().equals(s)) {
+                return template;
+            }
+        }
+        // throw an IllegalArgumentException or return null
+        throw new IllegalArgumentException("the given string doesn't match any Template.");
     }
 }
